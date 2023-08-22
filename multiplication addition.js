e1f2c6bc09b12b6249d30f9c7e5832efd6bf53fa
cssg1rl <107090440+cssg1rl@users.noreply.github.com>
2023-08-22 22:25:09 +0700

    Add files via upload

---
+++ multiplication addition.js
@@ -0,0 +1,23 @@
+  /*                                           
+
+
+  for (var i = 1; i < 11; i++) {
+    for (var j = 1; j < 11; j++) {
+    console.log(~~Math.pow(i, j));
+    }
+}
+
+ */
+
+for (var i = 1; i < 11; i++) {
+  for (var j = 1; j < 11; j++) {
+  console.log(i+'+'+j+'=', i + j);
+  }
+}
+
+
+for (var i = 1; i < 11; i++) {
+  for (var j = 1; j < 11; j++) {
+  console.log(i+'*'+j+'=', i * j);
+  }
+}
